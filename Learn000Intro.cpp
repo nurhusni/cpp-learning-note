e4d01b35468ba039f3045b6cf63057d8bab1ae37
Panji Ahmad Nurhusni <apanji@hotmail.co.id>
2021-10-09 14:24:27 +0700

    Added changes

--- Learn000Intro.cpp
+++ Learn000Intro.cpp
@@ -4,24 +4,38 @@ using namespace std;
 
 int main() 
 {
+	// 1
 	// Without using namespace std and \n for line break
 	std::cout << "Hello World!\n";
 
 	// Using namespace std and using endl
 	cout << "Let's start learning C++" << endl;
 
+	// Additional:
 	// Operator << is called overloaded operator
 	// Operators are like functions
-	// Above statement is equivalent to
 
-	// Data types
-	bool booleanType = true;
-	int wholeNumber = 10;
-	double decimal = 10.2;
-	char singleCharacter = 'a';
-	string multipleCharacters = "Learn C++";
+	// 2
+	// Data Types and Variables
+	// Variables are stored in memory, in either stack or heap
 
-	// Variables
+	// Primitive Data Types
+	bool booleanType = true; // 1 byte = 8 bit | 0 is false, else is true
+
+	short shortNumber; // 2 bytes
+	int wholeNumber = -10; // 4 bytes = 32 bits | from -(2^31) to 2^31
+	unsigned int positiveNumber = 10; // 4 bytes = 32 bits | from 0 to 2^32
+	long longNumber; // 4 bytes
+	long long longLongNumber; // 8 bytes
+
+	float shorterDecimal = 5.2f; // 4 bytes
+	double decimal = 10.2; // 8 bytes
+
+	char singleCharacter = 'a'; // 1 byte
+
+	// The size of data type can be found using `sizeof(datatype)` or `sizeof datatype`
+	cout << sizeof(wholeNumber) << endl; // prints 4
+
 	string firstName = "Panji";
 	int age = 22;
 
--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # C++ Note
 
-My note when learning C++. This covers everything from basic.
+My learning note when I learn C++. This covers everything from basic.
 
 ## Sources
 
-- [BroCode's C++ Full Course](https://www.youtube.com/watch?v=87SH2Cn0s9A) (main source).
+- [BroCode's C++ Full Course video](https://www.youtube.com/watch?v=87SH2Cn0s9A).
+- [The Cherno's C++ playlist](https://www.youtube.com/playlist?list=PLlrATfBNZ98dudnM48yfGUldqGD0S4FFb).
